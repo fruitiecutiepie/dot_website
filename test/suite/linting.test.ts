08fbdabdbde875291d6984445cffc863329d4dcc
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2024-10-07 13:47:35 +1100

    DW-120: iterating to fix broken diagnostic test

--- test/suite/linting.test.ts
+++ test/suite/linting.test.ts
@@ -13,27 +13,16 @@ import * as path from 'path';
 suite('Linting Tests for .website Files', async () => {
   const testFilePath = path.join(__dirname, '..', '..', '..', 'test', 'fixtures', 'sample.website');
 
-  await vscode.languages.onDidChangeDiagnostics(() => {
-    console.log('Diagnostics changed');
-  });
-
   test('Linting should report an error for multi-line content', async () => {
     const dotWebsiteExtension = vscode.extensions.getExtension('ftctpi.dot-website');
     if (dotWebsiteExtension && !dotWebsiteExtension.isActive) {
       await dotWebsiteExtension.activate();
       console.log('Activated extension');
-    } else {
-      throw Error('extension not activated');
     }
-
-    vscode.extensions.all.forEach(ext => {
-      console.log(`${ext.id}: ${ext.isActive ? 'Active' : 'Inactive'}`);
-    });
 
     const uri = vscode.Uri.file(testFilePath);
     const document = await vscode.workspace.openTextDocument(uri);
     await vscode.window.showTextDocument(document);
-    await vscode.commands.executeCommand('editor.action.formatDocument');
 
     // Set content with multiple lines
     const edit = new vscode.WorkspaceEdit();
@@ -44,21 +33,21 @@ suite('Linting Tests for .website Files', async () => {
     // Manually trigger linting is required on CI, probably due to headless mode
     // lint_document(document, vscode.languages.createDiagnosticCollection('dot-website'));
 
-    await new Promise(resolve => setTimeout(resolve, 1000));
     const diagnostics = vscode.languages.getDiagnostics(document.uri);
-    console.log('diagnostics 1', diagnostics);
-
-    // TODO: Mocking this for now, since 
-
     assert.strictEqual(diagnostics.length, 1);
     assert.strictEqual(diagnostics[0].message, 'File should only contain one line.');
   });
 
   test('Linting should report an error for invalid URL', async () => {
+    const dotWebsiteExtension = vscode.extensions.getExtension('ftctpi.dot-website');
+    if (dotWebsiteExtension && !dotWebsiteExtension.isActive) {
+      await dotWebsiteExtension.activate();
+      console.log('Activated extension');
+    }
+
     const uri = vscode.Uri.file(testFilePath);
     const document = await vscode.workspace.openTextDocument(uri);
     await vscode.window.showTextDocument(document);
-    await vscode.commands.executeCommand('editor.action.formatDocument');
 
     // Set content with invalid URL
     const edit = new vscode.WorkspaceEdit();
@@ -66,18 +55,21 @@ suite('Linting Tests for .website Files', async () => {
     await vscode.workspace.applyEdit(edit);
     await document.save();
 
-    await new Promise(resolve => setTimeout(resolve, 1000));
     const diagnostics = vscode.languages.getDiagnostics(document.uri);
-    console.log('diagnostics 2', diagnostics);
     assert.strictEqual(diagnostics.length, 1);
     assert.strictEqual(diagnostics[0].message, "Invalid URL: Must start with 'http://' or 'https://' and contain a valid address.");
   });
 
   test('Linting should pass for valid single-line URL', async () => {
+    const dotWebsiteExtension = vscode.extensions.getExtension('ftctpi.dot-website');
+    if (dotWebsiteExtension && !dotWebsiteExtension.isActive) {
+      await dotWebsiteExtension.activate();
+      console.log('Activated extension');
+    }
+
     const uri = vscode.Uri.file(testFilePath);
     const document = await vscode.workspace.openTextDocument(uri);
     await vscode.window.showTextDocument(document);
-    await vscode.commands.executeCommand('editor.action.formatDocument');
 
     // Set content with a valid URL
     const edit = new vscode.WorkspaceEdit();
@@ -85,19 +77,7 @@ suite('Linting Tests for .website Files', async () => {
     await vscode.workspace.applyEdit(edit);
     await document.save();
 
-    await new Promise(resolve => setTimeout(resolve, 1000));
     const diagnostics = vscode.languages.getDiagnostics(document.uri);
-    console.log('diagnostics 3', diagnostics);
     assert.strictEqual(diagnostics.length, 0);
   });
-
-  console.log('Installed extensions:', vscode.extensions.all.map(ext => {
-    console.log(`${ext.id}: ${ext.isActive ? 'Active' : 'Inactive'}`);
-  }));
-  const extension = vscode.extensions.getExtension('ftctpi.dot-website');
-    if (extension && !extension.isActive) {
-      await extension.activate();
-    }
-    
-
 });
